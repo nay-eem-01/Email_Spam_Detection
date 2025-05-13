535660088a70bde9c8decaf01ca031dcbdc88369
nay-eem-01 <ahmedmdnayeem05@gmail.com>
2025-05-14 04:11:08 +0600

    Fix deployment errors

--- app.py
+++ app.py
@@ -1,50 +1,47 @@
 import streamlit as st
 import pickle
-
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+# Download NLTK resources if not present
+try:
+    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+# Initialize the PorterStemmer
 ps = PorterStemmer()
 
-
 def transform_text(text):
+    # Text preprocessing
     text = text.lower()
     text = nltk.word_tokenize(text)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+    # Remove non-alphanumeric characters
+    y = [i for i in text if i.isalnum()]
 
-    text = y[:]
-    y.clear()
+    # Remove stopwords and punctuation
+    y = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
 
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
+    # Stem the words
+    y = [ps.stem(i) for i in y]
 
     return " ".join(y)
 
-
+# Load vectorizer and model
 cv = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
+# Streamlit UI
 st.title("Email/SMS Spam Classifier")
-
 input_email = st.text_area("Enter the email")
 
 if st.button('Predict'):
-
     transformed_text = transform_text(input_email)
-
     vector_input = cv.transform([transformed_text])
 
     result = model.predict(vector_input)[0]
