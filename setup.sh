449f0dcf140c70300b43997af6b8d1641b701f77
nay-eem-01 <ahmedmdnayeem05@gmail.com>
2025-05-14 03:47:42 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
+# Create .streamlit config folder
 mkdir -p ~/.streamlit/
 
+# Create Streamlit config file
 echo "\
 [server]\n\
-port = $PORT\n\
+port = \$PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+# Download NLTK data listed in nltk.txt
+python -c "import nltk; [nltk.download(line.strip()) for line in open('nltk.txt')]"
